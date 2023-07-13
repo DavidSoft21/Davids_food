486bddf924ebec85ac0fdeb3c545de7e942ebd64
E. David Riascos M <37507053+DavidSoft21@users.noreply.github.com>
2023-07-13 14:36:21 -0500

    maquetado principal
    diseño pagina Home

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,11 @@ module.exports = {
   theme: {
     extend: {
       colors:{
-       
+        primary: '#f3c614',
+        secondary:'#353535'
+      },
+      height:{
+        '80' : '20rem'
       }
     },
   },
